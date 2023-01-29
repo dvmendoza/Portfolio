4825f30afeb852b867139414dc45c0e2e9cba785
Daniel Mendpoza <dan.v.mendoza@gmail.com>
2023-01-28 17:40:13 -0800

    update

--- src/utilities/commonUtils.js
+++ src/utilities/commonUtils.js
@@ -1,5 +1,7 @@
 import Home from '../PortfolioContainer/Home/Home'
 import AboutMe from '../PortfolioContainer/AboutMe/AboutMe'
+import Resume from '../PortfolioContainer/Resume/Resume'
+
 
 export const TOTAL_SCREENS = [
     {
@@ -10,6 +12,11 @@ export const TOTAL_SCREENS = [
         screen_name: "AboutMe",
         component: AboutMe,
     },
+    {
+        screen_name: "Resume",
+        component: Resume,
+    },
+
 ]
 
 export const GET_SCREEN_INDEX = (screen_name) => {
